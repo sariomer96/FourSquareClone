0acd7df1b4dea063b59b86de3d92592d9de212e5
omer <somersari.96@gmail.com>
2023-08-19 19:55:56 +0300

    navigation added

--- FourSquareClone/DetailsVC.swift
+++ FourSquareClone/DetailsVC.swift
@@ -9,7 +9,7 @@ import UIKit
 import Parse
 import MapKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController,MKMapViewDelegate{
     @IBOutlet weak var detailsImageView: UIImageView!
     
     @IBOutlet weak var detailsPlaceNameLabel: UILabel!
@@ -21,11 +21,49 @@ class DetailsVC: UIViewController {
     var chosenLongitude = Double()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         getDataFromParse()
+        detailsMapView.delegate = self
         // Do any additional setup after loading the view.
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+        
+        if pinView == nil {
+            pinView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            let button = UIButton(type: .detailDisclosure)
+            pinView?.rightCalloutAccessoryView = button
+        }else {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if self.chosenLongitude != 0.0 && self.chosenLatitude != 0.0 {
+            let requestLocation = CLLocation(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+            
+            CLGeocoder().reverseGeocodeLocation(requestLocation){ (placemarks,error) in
+                if let placemark = placemarks {
+                    if placemark.count > 0 {
+                        let mkPlaceMark = MKPlacemark(placemark: placemark[0])
+                        let mapItem = MKMapItem(placemark: mkPlaceMark)
+                        mapItem.name = self.detailsPlaceNameLabel.text
+                        
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+                        mapItem.openInMaps(launchOptions: launchOptions)
+                    }
+                }
+            }
+        }
+    }
 
     func getDataFromParse(){
         
@@ -90,5 +128,9 @@ class DetailsVC: UIViewController {
                 }
             }
         }
+        
+        
+        
+        
     }
 }
