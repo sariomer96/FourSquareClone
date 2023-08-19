1f3d602672161607e865f61bc89d172d506f8e94
omer <somersari.96@gmail.com>
2023-08-19 19:39:25 +0300

    get data from parse

--- FourSquareClone/DetailsVC.swift
+++ FourSquareClone/DetailsVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 import MapKit
 
 class DetailsVC: UIViewController {
@@ -15,11 +16,79 @@ class DetailsVC: UIViewController {
     @IBOutlet weak var detailsPlaceTypeLabel: UILabel!
     @IBOutlet weak var detailsMapView: MKMapView!
     @IBOutlet weak var detailsPlaceAtmosphereLabel: UILabel!
+    var chosenPlaceId = ""
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        getDataFromParse()
         // Do any additional setup after loading the view.
     }
     
 
+    func getDataFromParse(){
+        
+        let query = PFQuery(className: "Places")
+        
+        // OBJECTS
+        query.whereKey("objectId", equalTo: chosenPlaceId)
+        query.findObjectsInBackground {
+            (objects,error) in
+            if error != nil {
+                
+                
+            }else{
+                if objects != nil {
+                    let chosenPlaceObject = objects![0]
+                  
+                    if let placeName = chosenPlaceObject.object(forKey:"name" ) as? String {
+                        self.detailsPlaceNameLabel.text = placeName
+                    }
+                    if let placeType = chosenPlaceObject.object(forKey:"type" ) as? String {
+                        self.detailsPlaceTypeLabel.text = placeType
+                    }
+                    if let placeAtmosp = chosenPlaceObject.object(forKey:"atmosphere" ) as? String {
+                        self.detailsPlaceAtmosphereLabel.text = placeAtmosp
+                    }
+                    if let placeLatitude = chosenPlaceObject.object(forKey:"latitude" ) as? String {
+                        if let placeLatitudeDouble = Double(placeLatitude){
+                            self.chosenLatitude = placeLatitudeDouble
+                        }
+                    }
+                    if let placeLongitude = chosenPlaceObject.object(forKey:"longitude" ) as? String {
+                        if let placeLongitudeDouble = Double(placeLongitude){
+                            self.chosenLongitude = placeLongitudeDouble
+                        }
+                    }
+                    
+                    if let imageData = chosenPlaceObject.object(forKey: "image") as? PFFileObject{
+                        imageData.getDataInBackground{
+                            (data,error) in
+                            if error == nil {
+                                if data != nil {
+                                    self.detailsImageView.image = UIImage(data: data!)
+                                }
+                                
+                            }
+                        }
+                    }
+                    
+                    //MAPS
+                    
+                    let location = CLLocationCoordinate2D(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+                    
+                    let span = MKCoordinateSpan(latitudeDelta: 0.035, longitudeDelta: 0.035)
+                    let region = MKCoordinateRegion(center: location, span: span)
+                    self.detailsMapView.setRegion(region, animated : true)
+                    let annotation = MKPointAnnotation()
+                    
+                    annotation.coordinate = location
+                    annotation.title = self.detailsPlaceNameLabel.text!
+                    annotation.subtitle = self.detailsPlaceTypeLabel.text!
+                    self.detailsMapView.addAnnotation(annotation)
+                }
+            }
+        }
+    }
 }
--- FourSquareClone/PlacesVC.swift
+++ FourSquareClone/PlacesVC.swift
@@ -8,8 +8,48 @@
 import UIKit
 import Parse
 
-class PlacesVC: UIViewController {
-
+class PlacesVC: UIViewController , UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeNamerArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = placeNamerArray[indexPath.row]
+        return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    var placeNamerArray = [String]()
+    var placeIdArray = [String]()
+    var selectedPlaceId = ""
+    
+    func getDataFromParse(){
+        let query = PFQuery(className: "Places")
+        query.findObjectsInBackground{
+            (objects,error) in
+            if error != nil {
+                self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "ERROR")
+            }else {
+                if objects != nil{
+                    
+                    self.placeIdArray.removeAll()
+                    self.placeNamerArray.removeAll()
+                    
+                    for object in objects!{
+                        if let placeName = object.object(forKey: "name") as? String {
+                            if let placeId = object.objectId{
+                                
+                                self.placeNamerArray.append(placeName)
+                                self.placeIdArray.append(placeId)
+                            }
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +58,11 @@ class PlacesVC: UIViewController {
         
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutButtonClicked))
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getDataFromParse()
     }
     
     @objc func addButtonClicked() {
@@ -31,16 +76,33 @@ class PlacesVC: UIViewController {
             
             if error != nil {
                 
-                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
-                
-                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler : nil)
-                
-                alert.addAction(okButton)
-                self.present(alert,animated: true)
+                self.makeAlert(titleInput: "error", messageInput: error?.localizedDescription ?? "error")
             }else{
                 self.performSegue(withIdentifier: "toSignUpVC", sender: nil)
             }
         }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC"{
+            let destinationcVC = segue.destination as! DetailsVC
+            
+            destinationcVC.chosenPlaceId = selectedPlaceId
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPlaceId = placeIdArray[indexPath.row]
+        self.performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func makeAlert(titleInput:String,messageInput:String){
+        let alert = UIAlertController(title: titleInput, message:messageInput, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler : nil)
+        
+        alert.addAction(okButton)
+        self.present(alert,animated: true)
     }
 
 }
